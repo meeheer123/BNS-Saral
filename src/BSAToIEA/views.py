4c21e8389224c6e56d43c986ffe9e3f98fc38b45
=mihir <=mihir.pande22@pccoepune.org>
2024-07-09 19:46:19 +0530

    test

--- src/BSAToIEA/views.py
+++ src/BSAToIEA/views.py
@@ -21,128 +21,123 @@ def get_csv_path(filename: str) -> str:
     normalized_path = os.path.normpath(filename)
     
     try:
-        return os.path.join(current_dir, '..', 'files', normalized_path)
+        return os.path.join(current_dir, '..', '..', 'files', normalized_path)
     except Exception as e:
         print(f"Error occurred while constructing CSV path: {e}")
         return None
 
-def load_bnss_crpc_mapping() -> Dict[str, str]:
+def load_bsa_iea_mapping() -> Dict[str, str]:
     """
-    Reads an Excel file containing BNSS to CRPC mappings and constructs a dictionary where BNSS sections are keys
-    and CRPC sections are values.
+    Reads an Excel file containing BSA to IEA mappings and constructs a dictionary where BSA sections are keys
+    and IEA sections are values.
 
     Returns:
-    A dictionary where keys are BNSS sections and values are CRPC sections.
+    A dictionary where keys are BSA sections and values are IEA sections.
     """
     mapping = {}
-    excel_path = get_csv_path('bnss_to_crpc_mapping.xlsx')
+    excel_path = get_csv_path('bsa_to_iea_mapping.xlsx')
 
     wb = load_workbook(filename=excel_path, read_only=True)
     ws = wb.active
 
     for row in ws.iter_rows(min_row=2, values_only=True):
         if len(row) >= 4:
-            section_bnss, heading_bnss, section_crpc, heading_crpc = row[:4]
-            mapping[str(section_bnss).strip()] = str(section_crpc).strip()
+            section_bsa, section_iea, subject, bsa_data = row[:4]
+            mapping[section_bsa] = section_iea
         else:
             print(f"Row with unexpected number of columns: {row}")
 
     return mapping
 
-def load_bnss_extra_data(bnss: str) -> str:
+def load_bsa_extra_data(bsa: str) -> str:
     """
-    Reads an Excel file to find and return the corresponding heading for a given BNSS section.
+    Reads an Excel file to find and return the corresponding heading for a given BSA section.
 
     Parameters:
-        bnss (str): The BNSS section to look up in the Excel file.
+        bsa (str): The BSA section to look up in the Excel file.
 
     Returns:
-        str: The CRPC heading corresponding to the given BNSS section if found, otherwise an empty string.
+        str: The IEA heading corresponding to the given BSA section if found, otherwise an empty string.
     """
-    excel_path = get_csv_path('D:/ProjectWithMihir/BNS-Saral/files/bnss_to_crpc_mapping.xlsx')
+    excel_path = get_csv_path('bsa_to_iea_mapping.xlsx')
 
     try:
         wb = load_workbook(filename=excel_path, read_only=True)
         ws = wb.active
 
         for row in ws.iter_rows(min_row=2, values_only=True):
-            section_bnss, heading_bnss, section_crpc, heading_crpc = [str(cell).strip() if cell is not None else '' for cell in row[:4]]
-            if section_bnss == bnss:
-                return heading_crpc
+            section_bsa, section_iea, subject, bsa_data = row[:4]
+            if section_bsa == bsa:
+                return bsa_data
 
     except Exception as e:
         print(f"Error occurred while reading Excel file: {e}")
 
     return ""
 
-def load_crpc_extra_data(crpc: str) -> str:
+def load_iea_extra_data(iea: str) -> str:
     """
-    Reads an Excel file to find and return the heading corresponding to a given CRPC section.
+    Reads an Excel file to find and return the heading corresponding to a given IEA section.
 
     Parameters:
-    - crpc: The CRPC section to be searched in the Excel file.
+    - iea: The IEA section to be searched in the Excel file.
 
     Returns:
-    - The heading corresponding to the given CRPC section if found.
+    - The heading corresponding to the given IEA section if found.
     - An empty string if no match is found or an error occurs.
     """
-    excel_path = get_csv_path('D:/bns/temp folder/KnowYourLaw/files/bnss_to_crpc_mapping.xlsx')
+    excel_path = get_csv_path('bsa_to_iea_mapping.xlsx')
 
     try:
         wb = load_workbook(filename=excel_path, read_only=True)
         ws = wb.active
 
         for row in ws.iter_rows(min_row=2, values_only=True):
-            section_bnss, heading_bnss, section_crpc, heading_crpc = [str(cell).strip() if cell is not None else '' for cell in row[:4]]
+            section_bsa, section_iea, subject, bsa_data = row[:4]
             
-            if section_crpc == crpc:
-                return heading_crpc
+            if section_iea == iea:
+                return bsa_data
 
     except Exception as e:
         print(f"Error occurred while reading Excel file: {e}")
 
     return ""
 
-def find_crpc_from_bnss(bnss: str) -> Union[str, None]:
+def find_iea_from_bsa(bsa: str) -> Union[str, None]:
     """
-    Takes a BNSS section as input and returns the corresponding CRPC section based on a pre-defined mapping loaded from an Excel file.
-    
-    If the BNSS section maps to a hyphen ('-'), it returns "New Section".
+    Takes a BSA section as input and returns the corresponding IEA section based on a pre-defined mapping loaded from an Excel file.
     
     Parameters:
-    - bnss: A string representing a BNSS section.
+    - bsa: A string representing a BSA section.
     
     Returns:
-    - A string representing the corresponding CRPC section or "New Section" if the mapping is a hyphen ('-').
+    - A string representing the corresponding IEA section.
     """
-    mapping = load_bnss_crpc_mapping()
-    
-    if mapping.get(bnss) == '-':
-        return "New Section"
-    
-    return mapping.get(bnss)
+    mapping = load_bsa_iea_mapping()
+    print(mapping)
+    return mapping.get(bsa)
 
-def find_bnss_from_crpc(crpc: str) -> Optional[str]:
+def find_bsa_from_iea(iea: str) -> Optional[str]:
     """
-    Search for a BNSS section that corresponds to a given CRPC section by loading a mapping from an Excel file and iterating through it.
+    Search for a BSA section that corresponds to a given IEA section by loading a mapping from an Excel file and iterating through it.
 
     Parameters:
-    crpc (str): A string representing the CRPC section to be searched.
+    iea (str): A string representing the IEA section to be searched.
 
     Returns:
-    Optional[str]: The BNSS section corresponding to the given CRPC section if found, otherwise None.
+    Optional[str]: The BSA section corresponding to the given IEA section if found, otherwise None.
     """
-    mapping = load_bnss_crpc_mapping()
+    mapping = load_bsa_iea_mapping()
 
-    for bnss, mapped_crpc in mapping.items():
-        if mapped_crpc == crpc:
-            return bnss
+    for bsa, mapped_iea in mapping.items():
+        if mapped_iea == iea:
+            return bsa
         
     return None
 
 def home(request) -> Union[JsonResponse, render]:
     """
-    Handles POST requests to map sections between BNSS and CRPC codes.
+    Handles POST requests to map sections between BSA and IEA codes.
     
     Parameters:
         request: The HTTP request object containing method, POST data, etc.
@@ -156,21 +151,21 @@ def home(request) -> Union[JsonResponse, render]:
         section = request.POST.get('section', '').strip()
         code_type = request.POST.get('code_type', '').strip()
 
-        if code_type == 'crpc to bnss':
-            bnss_result = find_bnss_from_crpc(section)
-            if bnss_result:
-                bnss_data = load_bnss_extra_data(bnss_result)
-                return JsonResponse({'bnss': bnss_result, 'bnss_data': bnss_data})
+        if code_type == 'iea to bsa':
+            bsa_result = find_bsa_from_iea(section)
+            if bsa_result:
+                bsa_data = load_bsa_extra_data(bsa_result)
+                return JsonResponse({'bsa': bsa_result, 'bsa_data': bsa_data})
             else:
-                return JsonResponse({'bnss': 'BNSS section not found for given CRPC.', 'bnss_data': 'BNSS section not found for given CRPC.'})
-        elif code_type == 'bnss to crpc':
-            crpc_result = find_crpc_from_bnss(section)
-            if crpc_result:
-                bnss_data = load_bnss_extra_data(section) if crpc_result == "New Section" else load_crpc_extra_data(crpc_result)
-                return JsonResponse({'crpc': crpc_result, 'bnss_data': bnss_data})
+                return JsonResponse({'bsa': 'BSA section not found for given IEA.', 'bsa_data': 'BSA section not found for given IEA.'})
+        elif code_type == 'bsa to iea':
+            iea_result = find_iea_from_bsa(section)
+            if iea_result:
+                bsa_data = load_iea_extra_data(iea_result)
+                return JsonResponse({'iea': iea_result, 'bsa_data': bsa_data})
             else:
-                return JsonResponse({'crpc': 'CRPC section not found for given BNSS.', 'bnss_data': 'CRPC section not found for given BNSS.'}, status=404)
+                return JsonResponse({'iea': 'IEA section not found for given BSA.', 'bsa_data': 'IEA section not found for given BSA.'}, status=404)
         else:
-            return JsonResponse({'error': 'Please provide either CRPC or BNSS section number.'}, status=400)
+            return JsonResponse({'error': 'Please provide either IEA or BSA section number.'}, status=400)
 
-    return render(request, 'BNSSToCRPC/home.html')
+    return render(request, 'BSAToIEA/home.html')
